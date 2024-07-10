80a0d983b42d8eaf89ee410877c83ccc6be51d3a
Tejal <121215264+SantoryuSamurai@users.noreply.github.com>
2024-07-11 03:29:05 +0530

    removed carousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import HeroCarousel from "@/components/HeroCarousel"
 import Searchbar from "@/components/Searchbar"
 import Image from "next/image"
 import { getAllProducts } from "@/lib/actions"
@@ -10,33 +9,29 @@ const Home = async () => {
   return (
     <>
       <section className="px-6 md:px-20 py-24">
-        <div className="flex max-xl:flex-col gap-16">
-          <div className="flex flex-col justify-center"> 
-            <p className="small-text">
-              Smart Shopping Starts Here:
-              <Image 
-                src="/assets/icons/arrow-right.svg"
-                alt="arrow-right"
-                width={16}
-                height={16}
-              />
-            </p>
+  <div className="flex flex-col justify-center">
+    <p className="small-text">
+      Smart Shopping Starts Here:
+      <Image 
+        src="/assets/icons/arrow-right.svg"
+        alt="arrow-right"
+        width={16}
+        height={16}
+      />
+    </p>
 
-            <h1 className="head-text">
-              Unleash the Power of
-              <span className="text-primary"> PriceWise</span>
-            </h1>
+    <h1 className="head-text">
+      Unleash the Power of
+      <span className="text-primary"> PriceWise</span>
+    </h1>
 
-            <p className="mt-6">
-            Powerful, self-serve product and growth analytics to help you maximize savings and retain more.
-            </p>
+    <p className="mt-6">
+      Track Your Amazon Products with PriceWise for Smarter Shopping and Bigger Savings!
+    </p>
+    <Searchbar />
+  </div>
+</section>
 
-            <Searchbar />
-          </div>
-
-          <HeroCarousel />
-        </div>
-      </section>
 
       <section className="trending-section">
         <h2 className="section-text">Searched Products</h2>
